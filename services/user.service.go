fc9aee9d126decff8092ac3cd66cc61ff1b81e3b
Büşra <busraoran043@gmail.com>
2022-10-04 17:46:30 +0300

    first commit

---
+++ services/user.service.go
@@ -0,0 +1,13 @@
+package services
+
+import (
+	"example.com/GoTutorials/models"
+)
+
+type UserService interface {
+	CreateUser(*models.User) error
+	GetUser(*string) (*models.User, error)
+	GetAll() ([]*models.User, error)
+	UpdateUser(*models.User) error
+	DeleteUser(*string) error
+}
---
+++ tutorial.go
@@ -0,0 +1,35 @@
+package main
+
+/*
+func main() {
+	fmt.Println("Welcome to my game!")
+	// burada Go dili her değişken için tip belirtmek gereksiz diye düşündüğünden biz
+	//değişkenleri satır 9daki gibi de belirtebiliriz
+	//istemesek var name string = "Hamza" da diyebilirdik
+	//fakat bu kullanım önerilen kullanım değil
+
+	// %v stands for value
+	//Printf means print with formatting
+	//fmt.Printf("Hello %v, your age is %v how are you?", name, age)
+
+	var name string
+	// we can just create the variable without initializing it
+	fmt.Printf("Please enter your name: ")
+	fmt.Scan(&name)
+
+	fmt.Printf("Hello %v, welcome to my game :)\n", name)
+
+	fmt.Printf("Please enter your age: ")
+	var age uint
+
+	fmt.Scan(&age)
+
+	if age > 15 {
+		fmt.Println("You can playyy")
+	} else {
+		fmt.Println("Soory to say but you cannot play!")
+		return
+	}
+
+}
+*/
